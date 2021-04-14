1566a4b63cec9197aaf4d61b1e33100c47913565
elysemontano <elysemontano@gmail.com>
2021-04-14 12:04:18 -0700

    navbar functional except signout and mock data created

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -13,8 +13,18 @@ import NotFound from './pages/NotFound';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MockItems from './MockItems';
+import MockParties from './MockParties';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      parties: MockParties,
+      items: MockItems
+    }
+  }
+
   render() {
     const {
       logged_in,
@@ -23,9 +33,17 @@ class App extends React.Component {
       sign_in_route,
       sign_out_route,
     } = this.props;
+    console.log(this.state.parties);
+    console.log(this.state.items);
+    console.log(current_user);
     return (
       <Router>
-        <Header />
+        <Header logged_in= { logged_in }
+                current_user= { current_user }
+                sign_in_route= { sign_in_route }
+                sign_out_route= { sign_out_route }
+                new_user_route= { new_user_route }
+        />
         <h1>Hello Reunion!</h1>
         <Switch>
           {!logged_in && <Route path='/' component={LoggedOutHome} />}
---
+++ app/javascript/components/MockItems.js
@@ -0,0 +1,33 @@
+const items = [
+  {
+    id: 1,
+    item_bringing: 'cups',
+    allergies: 'plastic',
+    party_id: 1,
+    user_id: 2
+  },
+  {
+    id: 2,
+    item_bringing: 'platess',
+    allergies: 'none',
+    party_id: 1,
+    user_id: 2
+  },
+  {
+    id: 3,
+    item_bringing: 'leaves',
+    allergies: 'pollen',
+    party_id: 2,
+    user_id: 3
+  },
+  {
+    id: 4,
+    item_bringing: 'rope',
+    allergies: 'no',
+    party_id: 2,
+    user_id: 1
+  }
+]
+
+
+export default items
