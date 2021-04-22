55b62d82527510257bc0f8906941c416cf7aa705
kevinnarbas <kevinnarbas@gmail.com>
2021-04-22 13:23:28 -0700

    adds styling to event new page

--- app/javascript/components/pages/EventNew.js
+++ app/javascript/components/pages/EventNew.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { Redirect } from 'react-router-dom';
+import './EventNew.scss';
 
 class EventNew extends Component {
   constructor(props) {
@@ -31,56 +32,58 @@ class EventNew extends Component {
     const { form, submitted } = this.state;
     return (
       <>
-        <h1>Create Event</h1>
-        <Form>
-          <Row form>
-            <Col md={6}>
-              <FormGroup>
-                <Label for='party_name'>Event Name</Label>
-                <Input
-                  type='text'
-                  name='party_name'
-                  placeholder='Enter Event Name'
-                  value={form.party_name}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-              <FormGroup>
-                <Label for='party_start_time'>When?</Label>
-                <Input
-                  type='text'
-                  name='party_start_time'
-                  placeholder='Enter Start Time'
-                  value={form.party_start_time}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <FormGroup>
-            <Label for='location'>Where?</Label>
-            <Input
-              type='text'
-              name='location'
-              placeholder="Where's the party??"
-              value={form.location}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for='description'>Description</Label>
-            <Input
-              type='textarea'
-              name='description'
-              placeholder="What's going on at the party???"
-              value={form.description}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <Button onClick={this.handleSubmit}>Continue</Button>
-        </Form>
+        <h1 className='h1-styles'>Create Event</h1>
+        <Col className='column-text' md={{ size: 10, offset: 1 }}>
+          <Form>
+            <Row form>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for='party_name'>Event Name</Label>
+                  <Input
+                    type='text'
+                    name='party_name'
+                    placeholder='Enter Event Name'
+                    value={form.party_name}
+                    onChange={this.handleChange}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for='party_start_time'>When?</Label>
+                  <Input
+                    type='text'
+                    name='party_start_time'
+                    placeholder='Enter Start Time'
+                    value={form.party_start_time}
+                    onChange={this.handleChange}
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <FormGroup>
+              <Label for='location'>Where?</Label>
+              <Input
+                type='text'
+                name='location'
+                placeholder="Where's the party??"
+                value={form.location}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for='description'>Description</Label>
+              <Input
+                type='textarea'
+                name='description'
+                placeholder="What's going on at the party???"
+                value={form.description}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <Button onClick={this.handleSubmit}>Continue</Button>
+          </Form>
+        </Col>
         {this.state.submitted && <Redirect to='/itemnew' />}
       </>
     );
