2e48405e10c4bc7030f78e622f32449a30ac3a74
kevinnarbas <kevinnarbas@gmail.com>
2021-04-23 14:56:49 -0700

    fixes PascalCase on test files

--- app/javascript/components/pages/EventNew.test.js
+++ app/javascript/components/pages/EventNew.test.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import EventNew from './eventnew';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import EventNew from './EventNew';
 
-Enzyme.configure({ adapter: new Adapter()})
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('When eventnew renders', () => {
-    it('displays create event heading', () => {
-      const currentUser= {id: 1}
-      const eventNew = shallow(<EventNew createNewEvent={()=>{}}
-        current_user={currentUser} />);
-  
-      const editHeading = eventNew.find('h1');
-  
-      expect(editHeading.text()).toEqual('Create Event');
-    });
+  it('displays create event heading', () => {
+    const currentUser = { id: 1 };
+    const eventNew = shallow(
+      <EventNew createNewEvent={() => {}} current_user={currentUser} />
+    );
+
+    const editHeading = eventNew.find('h1');
+
+    expect(editHeading.text()).toEqual('Create Event');
   });
+});
